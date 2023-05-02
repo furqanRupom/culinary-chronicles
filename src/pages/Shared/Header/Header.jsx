04672fb7ec031eba8cee60b9c0f8e8c037b2d131
rupom <furqanrupom978@gmail.com>
2023-05-02 10:46:47 +0600

    added all necessary pages and components for assignment

---
+++ src/pages/Shared/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h3 className="text-4xl font-Nunito font-bold text-red-400">
+                this is our header
+            </h3>
+        </div>
+    );
+};
+
+export default Header;
