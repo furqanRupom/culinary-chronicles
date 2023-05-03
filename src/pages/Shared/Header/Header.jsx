107a77c6cc0e4b0a2f85a7ba0cae79090ee5787d
rupom <furqanrupom978@gmail.com>
2023-05-03 19:45:27 +0600

    added ActiveLink and style when any link is active

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -6,6 +6,7 @@ import { BiRightArrow } from "react-icons/bi";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { ChefContext } from "../../../Providers/AuthProviders";
+import ActiveLink from "../../../ActiveLink/ActiveLink";
 const Header = () => {
   const { toggle, setToggle } = useContext(ChefContext);
   return (
@@ -13,7 +14,6 @@ const Header = () => {
       <header className="font-Nunito relative  w-full lg:flex justify-between items-center px-4 bg-gray-100">
         <div className="flex justify-between  items-center px-2 py-5">
           <div className=" text-xl md:text-3xl font-semibold">
-          
             <h2 className="flex ">
               Culinary
               <TbChefHat />
@@ -62,19 +62,38 @@ const Header = () => {
             <hr />
             <div>
               <nav className="flex flex-col px-5 py-4 text-2xl space-y-3">
-                <Link className="hover:text-red-400 duration-200 w-fit" to="/">
-                  Home
-                </Link>
-                <Link className="hover:text-red-400 duration-200 w-fit" to="/">
-                  The Experience
-                </Link>
-                <Link className="hover:text-red-400 duration-200 w-fit" to="/">
-                  Our Chefs
-                </Link>
+                <ActiveLink
+                  className="w-fit"
+                  to="/"
+                >
+                  <button className="hover:text-red-400 duration-200">Home</button>
 
-                <Link className="hover:text-red-400 duration-200 w-fit" to="/">
-                  Contact us
-                </Link>
+                  </ActiveLink>
+                <ActiveLink
+                  className="w-fit"
+                  to="/experience"
+                >
+                  <button className="hover:text-red-400 duration-200">The Experience</button>
+
+
+                </ActiveLink>
+                <ActiveLink
+                  className="w-fit"
+                  to="/chefs"
+                >
+                  <button className="hover:text-red-400 duration-200">Our Chefs</button>
+
+
+                </ActiveLink>
+
+                <ActiveLink
+                  className="w-fit"
+                  to="/contact"
+                >
+                  <button className="hover:text-red-400 duration-200">Contact us</button>
+
+
+                </ActiveLink>
               </nav>
               <hr className="mt-4" />
               <button className=" text-center mt-5 flex items-center text-2xl space-x-1 justify-center w-full  bg-gray-200 hover:bg-red-100 rounded-full max-w-md mx-auto px-2 py-2 group">
@@ -88,19 +107,38 @@ const Header = () => {
         </div>
         <div className="hidden lg:block">
           <nav className="flex  px-5 py-4 lg:text-md xl:text-xl lg:space-x-4  xl:space-x-8 items-center">
-            <Link className="hover:text-red-400 duration-200 w-fit" to="/">
-              Home
-            </Link>
-            <Link className="hover:text-red-400 duration-200 w-fit" to="/">
-              The Experience
-            </Link>
-            <Link className="hover:text-red-400 duration-200 w-fit" to="/">
-              Our Chefs
-            </Link>
+            <ActiveLink
+              className="w-fit"
+              to="/"
+            >
+              <button className="hover:text-red-400 duration-200">Home</button>
 
-            <Link className="hover:text-red-400 duration-200 w-fit" to="/">
-              Contact us
-            </Link>
+              </ActiveLink>
+            <ActiveLink
+              className="w-fit"
+              to="/experience"
+            >
+              <button className="hover:text-red-400 duration-200">The Experience</button>
+
+
+            </ActiveLink>
+            <ActiveLink
+              className="w-fit"
+              to="/chef"
+            >
+              <button className="hover:text-red-400 duration-200">Our Chefs</button>
+
+
+            </ActiveLink>
+
+            <ActiveLink
+              className="w-fit"
+              to="/contact"
+            >
+              <button className="hover:text-red-400 duration-200">Contact us</button>
+
+
+            </ActiveLink>
           </nav>
         </div>
         <div>
