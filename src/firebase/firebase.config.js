40b3299ad2ac19b50a8675349e4f84dea843b00d
rupom <furqanrupom978@gmail.com>
2023-05-04 03:19:39 +0600

    added all secret information of  firebase auth in env local file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDwU161LLeIqJVtGkYqBc_mi57-HT-iaGY",
-  authDomain: "culinary-chronicles-auth.firebaseapp.com",
-  projectId: "culinary-chronicles-auth",
-  storageBucket: "culinary-chronicles-auth.appspot.com",
-  messagingSenderId: "148833231462",
-  appId: "1:148833231462:web:22250f82b592555115c3a7"
+  // apiKey: "AIzaSyDwU161LLeIqJVtGkYqBc_mi57-HT-iaGY",
+  // authDomain: "culinary-chronicles-auth.firebaseapp.com",
+  // projectId: "culinary-chronicles-auth",
+  // storageBucket: "culinary-chronicles-auth.appspot.com",
+  // messagingSenderId: "148833231462",
+  // appId: "1:148833231462:web:22250f82b592555115c3a7"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
