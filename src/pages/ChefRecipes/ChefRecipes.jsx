516042a829bcf7e2ab6ba9c20477db4848751653
rupom <furqanrupom978@gmail.com>
2023-05-05 15:36:00 +0600

    we forget to add rating on chef recipe section which is now added

--- src/pages/ChefRecipes/ChefRecipes.jsx
+++ src/pages/ChefRecipes/ChefRecipes.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import { FaHeart } from "react-icons/fa";
+import { FaHeart, FaRegClock } from "react-icons/fa";
 import toast, { Toaster } from "react-hot-toast";
 import { orange500 } from "./ChefRecipes.module.css";
 import LazyLoad from "react-lazyload";
+import { FaStar } from 'react-icons/fa';
 const ChefRecipes = () => {
   const {
     name,
@@ -69,47 +70,56 @@ const ChefRecipes = () => {
       </div>
 
       {/* Recipes */}
-      <div className="flex flex-col items-center py-8  max-w-7xl mx-auto">
-        <h3 className="text-4xl font-bold text-red-400 py-5">Recipes</h3>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-4">
-          {recipes.map((recipe) => (
-            <div
-              key={recipe.recipe_name}
-              className="bg-white shadow rounded-lg overflow-hidden w-[400px] md:w-auto"
-            >
-              <div className="md:h-auto">
-                <LazyLoad height={200}>
-                  <img
-                    src={recipe.recipe_image}
-                    alt={recipe.recipe_name}
-                    className=" md:w-full h-[48] md:h-[300px] object-cover"
-                  />
-                </LazyLoad>
-              </div>
-              <div className="p-4">
-                <h4 className="text-lg font-bold">{recipe.recipe_name}</h4>
-                <p className="text-sm">{recipe.recipe_description}</p>
-                <div className="mt-4 flex justify-between items-center">
-                  <p className="text-sm">{recipe.difficulty}</p>
-                  <p className="text-sm">
-                    {recipe.prep_time} prep / {recipe.cook_time} cook
-                  </p>
-                  <div
-                    onClick={() => toggleFavorite(recipe)}
-                    className="cursor-pointer"
-                  >
-                    <FaHeart
-                      size={20}
-                      color={isFavorite(recipe) ? "tomato" : "gray"}
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+      <div className="flex flex-col items-center py-8 max-w-7xl mx-auto">
+  <h3 className="text-4xl font-bold text-red-400 py-5">Recipes</h3>
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-4">
+    {recipes.map((recipe) => (
+      <div
+        key={recipe.recipe_name}
+        className="bg-white shadow rounded-lg overflow-hidden w-full md:w-auto"
+      >
+        <div className="md:h-auto">
+          <LazyLoad height={200}>
+            <img
+              src={recipe.recipe_image}
+              alt={recipe.recipe_name}
+              className="w-full  h-[400px] md:h-[300px] object-cover"
+            />
+          </LazyLoad>
+        </div>
+        <div className="p-4">
+          <h4 className="text-lg font-bold">{recipe.recipe_name}</h4>
+          <p className="text-sm">{recipe.recipe_description}</p>
+          <div className="mt-4 flex justify-between items-center">
+
+  <div className="flex items-center">
+    <FaRegClock size={18} color="#555" />
+    <p className="text-sm ml-2 font-medium">{recipe.prep_time} prep</p>
+    <span className="text-sm mx-2 font-medium text-gray-400">/</span>
+    <FaRegClock size={18} color="#555" />
+    <p className="text-sm ml-2 font-medium">{recipe.cook_time} cook</p>
+  </div>
+  <div onClick={() => toggleFavorite(recipe)} className="cursor-pointer">
+    <FaHeart size={20} color={isFavorite(recipe) ? "tomato" : "gray"} />
+  </div>
+</div>
+  <div className="flex items-center mt-5 w-full space-x-5">
+    <div className="flex items-center justify-between">
+      {[...Array(4)].map((_, i) => (
+        <FaStar key={i} size={18} color="gold" />
+      ))}
+      {[...Array(1)].map((_, i) => (
+        <FaStar key={i} size={18} color="gray" />
+      ))}
+    </div>
+    <p className="text-sm ml-2 font-medium">cook level : {recipe.difficulty}</p>
+  </div>
+
         </div>
       </div>
-
+    ))}
+  </div>
+</div>
       <Toaster
         containerStyle={{
           top: 100,
